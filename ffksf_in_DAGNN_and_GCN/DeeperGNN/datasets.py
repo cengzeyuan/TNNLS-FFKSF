283e53bdbd65acbfa7d0e30671da9f63e8db1a43
cengzeyuan <48182127+cengzeyuan@users.noreply.github.com>
2023-03-28 00:57:15 +0800

    Update datasets.py

--- ffksf_in_DAGNN_and_GCN/DeeperGNN/datasets.py
+++ ffksf_in_DAGNN_and_GCN/DeeperGNN/datasets.py
@@ -5,8 +5,7 @@ import torch_geometric.transforms as T
 
 
 def get_planetoid_dataset(name, normalize_features=False, transform=None):
-    # path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', name)
-    path = osp.join('/data0/zzy/DeeperGNN/data', name)
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', name)
     dataset = Planetoid(path, name)
 
     if transform is not None and normalize_features:
@@ -20,24 +19,8 @@ def get_planetoid_dataset(name, normalize_features=False, transform=None):
 
 
 def get_coauthor_dataset(name, normalize_features=False, transform=None):
-    # path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', name)
-    path = osp.join('/data0/zzy/DeeperGNN/data', name)
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', name)
     dataset = Coauthor(path, name)
-    
-    if transform is not None and normalize_features:
-        dataset.transform = T.Compose([T.NormalizeFeatures(), transform])
-    elif normalize_features:
-        dataset.transform = T.NormalizeFeatures()
-    elif transform is not None:
-        dataset.transform = transform
-
-    return dataset
-
-
-def get_amazon_dataset(name, normalize_features=False, transform=None):
-    # path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', name)
-    path = osp.join('/data0/zzy/DeeperGNN/data', name)
-    dataset = Amazon(path, name)
     
     if transform is not None and normalize_features:
         dataset.transform = T.Compose([T.NormalizeFeatures(), transform])
